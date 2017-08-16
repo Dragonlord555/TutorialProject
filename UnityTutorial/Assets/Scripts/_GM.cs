59261f0a507cb79a777f0a0f6b3dfd3d6085b918
Jack Tugby <masterjackt@hotmail.co.uk>
2017-08-16 20:24:42 +0100

    Added Stuff

--- UnityTutorial/Assets/Scripts/_GM.cs
+++ UnityTutorial/Assets/Scripts/_GM.cs
@@ -8,18 +8,12 @@ public class _GM : MonoBehaviour
 	void Start()
 	{
 		//This is the start function
-<<<<<<< HEAD
-=======
 		/*This Is Start*/
->>>>>>> 96bdc82bfba48588090dbc86765c7ba65fd18bea
 	}
 	
 	void Update()
 	{
 		//This is the update function
-<<<<<<< HEAD
-=======
 		/*This Is Update*/
->>>>>>> 96bdc82bfba48588090dbc86765c7ba65fd18bea
 	}
 }
