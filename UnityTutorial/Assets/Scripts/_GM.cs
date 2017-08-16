ee2a3fcd0f6c076b894dc77fbbe6ce27b3e1d443
Jack Tugby <masterjackt@hotmail.co.uk>
2017-08-16 20:04:52 +0100

    Added comments and UI stuff
    ...

--- UnityTutorial/Assets/Scripts/_GM.cs
+++ UnityTutorial/Assets/Scripts/_GM.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class _GM : MonoBehaviour
 {
-	
 	void Start()
 	{
-		
+		//This is the start function
 	}
-	
 	
 	void Update()
 	{
-		
+		//This is the update function
 	}
 }
