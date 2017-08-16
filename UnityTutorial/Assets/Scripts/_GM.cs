edd939d1dfe27535e7dc1cdd8063160b91b8b953
Jack Tugby <masterjackt@hotmail.co.uk>
2017-08-16 21:33:44 +0100

    Added stuff

---
+++ UnityTutorial/Assets/Scripts/_GM.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _GM : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    void Awake()
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Update()
+    {
+
+    }
+}
